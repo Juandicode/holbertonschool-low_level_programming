b390d6f9e47e73616d7c2317f1c3c09dbd91c554
Juandicode <9542@holbertonstudents.com>
2024-06-13 14:55:54 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a piece of art with proper grammar, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        printf("\"With proper grammar, but the outcome is a piece of art,\"\n");
+
+        return (0);
+}
