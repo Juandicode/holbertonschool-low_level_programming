390f8138942efffc1261b067e4ecd8c33fccd08e
Juandicode <9542@holbertonstudents.com>
2024-06-13 14:28:49 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
-#include <stdio.h>
-puts("Programming is like building a multilingual puzzle");
+gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+su student_jail -c 'timeout 30 bash -c '"'"'gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c '"'"'
