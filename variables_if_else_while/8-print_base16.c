3ab378c2d87bb795cdb683d0e53a64760fb77285
juandicode <juandiegomhs@gmail.com>
2024-06-17 17:05:07 +0000

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i <= 15; i++)
-{
-if (i < 10)
-putchar(i + '0');
-else
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else
+			putchar('\n');
+	}
+	return (0);
 }
