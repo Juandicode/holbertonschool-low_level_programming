044b07ecd8c8d3ff3bf692f84280d21531a75121
juandicode <juandiegomhs@gmail.com>
2024-06-17 12:49:50 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char R;
+
+	for (R = 'a'; R <= 'z'; R++)
+		putchar(R);
+
+	putchar('\n');
+	return (0);
+}
