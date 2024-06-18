22ed2ff95247da85eea60fa51d1be1c13e6e167d
Juandicode <juandiegomhs@gmail.com>
2024-06-18 13:17:33 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * Return: Always 0
  */
+
+
 void print_alphabet(void)
 {
-char alphabet;
+	char letter = 'a';
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-_putchar('\n');
+	while (letter != '{')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
