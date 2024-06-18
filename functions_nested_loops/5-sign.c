a46cb0a5b3b36b4519294b0eeffeb1b9c88fa061
Juandicode <juandiegomhs@gmail.com>
2024-06-18 14:27:01 +0000

    4-isalpha.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Entry Point
+ * Return: ???
+ * Description: this is a description
+ * @n: nnn
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+}
+}
