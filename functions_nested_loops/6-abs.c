3c74dfdf134af36894c8c5e37aea92988de7aace
Juandicode <juandiegomhs@gmail.com>
2024-06-18 15:05:01 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry Point
+ * Return: p; -p
+ * Description: this is a description
+ * @p: pp
+ */
+int _abs(int p)
+{
+	if (p >= 0)
+		return (p);
+	else
+		return (-p);
+}
