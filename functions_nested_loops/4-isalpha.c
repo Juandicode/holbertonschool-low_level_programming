d105113148b618862686e776f950f564e1c6ec6d
Juandicode <juandiegomhs@gmail.com>
2024-06-18 14:05:19 +0000

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *  _islower - Entry Point
+ * _isalpha - Entry Point
  * Return: Always 0
  * Description: this is a description
- * @c: also c
+ * @c: ccc
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c > 'a' && c <= 'z')
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
 		return (1);
 	else
 		return (0);
