a612ac44e787f9631f96f39b2b874979a8830467
Juandicode <juandiegomhs@gmail.com>
2024-06-18 15:08:40 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _islower - Entry Point
+ * Return: Always 0
+ * Description: this is a description
+ * @c: also c
+ */
+int print_last_digit(int u)
+{
+	int lg = u % 10;
+	if (lg < 0)
+		lg = -lg;
+	char ff = '0' + lg;
+	_putchar(ff);
+	
+		return (lg);
+}
