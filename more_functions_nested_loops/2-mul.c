1039ebabe2395d826e5df90a69bb27fffa2db3cd
Juandicode <juandiegomhs@gmail.com>
2024-06-18 19:37:50 +0000

    2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * mul- Entry point
+ * Description: 'Esto es una descripcion'
+ * @a: a
+ * @b: b
+ * Return: int
  */
-int main(void)
+int mul(int a, int b)
 {
-printf("%d\n", mul(98, 1024));
-printf("%d\n", mul(-402, 4096));
-return (0);
+return (a * b);
 }
