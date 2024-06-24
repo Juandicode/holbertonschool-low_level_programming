b58a5e99aeb5cde9a3b458778ca8947aada0afa5
juandicode <juandiegomhs@gmail.com>
2024-06-24 16:17:44 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,5 +9,13 @@ void puts2 (char *str)
 
 	v1 =0;
 
-	while ((str[v1]) != '\0')
-	{
+		while ((str[v1]) != '\0')
+		{
+			v1++;
+		}
+		for (v2 = 0; v2 < v1; v2 += 2)
+		{
+			_putchar(str[v2]);
+		}
+		_putchar('\n');
+}		
