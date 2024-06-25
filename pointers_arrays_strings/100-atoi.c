38f462106daae50f97901f72c4fd8912c8b44c1e
juandicode <juandiegomhs@gmail.com>
2024-06-25 17:19:15 +0000

    100 bro

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,14 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int j = 2;
-	int number = 0;
+
 	int sign = 1;
-	int quant_numbers = 0;
-	int multiplier = 1;
+	
+	
 
 	for ( ; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
 			sign = -sign;
 		else if (s[i] >= 48 && s[i] <= 57)
-		{
-			if (s[i + 1] >= 48 && s[i + 1] <= 57)
-				quant_numbers++;
-			else
-			{
-				quant_numbers++;
-				break;
-			}
-		}
-	}
-}	
+}
