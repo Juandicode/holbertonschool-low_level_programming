6d8509d2e4c827a7cbca7277272695f751ac5709
juandicode <juandiegomhs@gmail.com>
2024-06-26 02:13:08 +0000

    7 bruhh

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -35,3 +35,15 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 
 char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
