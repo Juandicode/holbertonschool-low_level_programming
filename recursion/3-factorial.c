c76c175d02abf348e080631c8bef8219e70f4d50
juandicode <juandiegomhs@gmail.com>
2024-06-27 14:28:22 +0000

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* factorial - descripcion
+* @n: string
+* Return: retorna el factorial
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
