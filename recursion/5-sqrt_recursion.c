02c23abf86ce4ff9e0c31b9c582770fc462a9cd0
juandicode <juandiegomhs@gmail.com>
2024-06-29 15:22:36 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,26 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find te square root of
- * Return: the natural square root of n , or -1 if no natural square root
+ * findroot - encuentra la raiz cuadrade de un numero
+ * @n: Numero
+ * @v1: Number to be powered by itself
+ * Return: Number
+ */
+int froot(int v1, int n)
+{
+	if (v1 * v1 == n)
+		return (v1);
+	else if (v1 * v1 < n)
+		return (froot(v1 + 1, n));
+	return (-1);
+}
+/**
+ * _sqrt_recursion - encuentra la raiz cuadrada de un numero
+ * @v2: Numero
+ * Return: Numero
  */
 int _sqrt_recursion(int n)
+{
+	int v2 = 0;
 
-	if (n = x * x)
-		return (x);
+	return (froot(v2, n));
+}
