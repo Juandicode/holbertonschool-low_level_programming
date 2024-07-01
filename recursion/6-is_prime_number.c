526977939ba3f675096c6f2877aa6168c286f054
juandicode <juandiegomhs@gmail.com>
2024-07-01 00:24:46 +0000

    1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,1 +1,28 @@
+#include "main.h"
+/**
+ * primo - Divides number
+ * @num: Numero que va adividir
+ * @n: Number divided
+ * Return: Resultado
+ */
+int primo(int num, int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == num)
+		return (1);
+	if (n % num == 0)
+		return (0);
 
+	return (primo(num + 1, n));
+}
+/**
+ * is_prime_number - mira si es numero primo
+ * @n: Number
+ * Return: 1 (Success) - 0 (Otherwise)
+ */
+int is_prime_number(int n)
+{
+
+	return (primo(2, n));
+}
