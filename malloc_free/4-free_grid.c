e2aba0006205dac7024c6b986a0303e5e2210a6c
juandicode <juandiegomhs@gmail.com>
2024-07-05 21:30:48 +0000

    1

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * free_grid - Frees the matrix previously made
+ * @grid: Pointer to a pointer where is the matrix
+ * @height: Height of the matrix
+ * Return: None
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0 < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
