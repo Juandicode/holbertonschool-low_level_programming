78e351822864b60ac32289496a228cd42f8456eb
Juandicode <juandiegomhs@gmail.com>
2024-07-08 14:00:00 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
 #include "main.h"
 #include "length.c"
 /**
  * *string_nconcat - Concatenates two strings
  * @s1: String 1
  * @s2: String 2
- * @n: unsigned int 
+ * @n: unsigned int
  * Return: String concatenated
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -27,24 +28,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > lens2)
 		n = lens2;
 	/* espacio dinamico de str */
-	newstr = malloc((lens1 + n + 1));
+	str = malloc((lens1 + n + 1));
 	/* Malloc  error */
-	if (wstr == NULL)
+	if (str == NULL)
 		return (NULL);
 	/* Copiamos String1 */
 	while (y < lens1)
 	{
-		newstr[y] = s1[y];
+		str[y] = s1[y];
 		y++;
 	}
 	/* Copiamos String2 */
 	while (b < n)
 	{
-		newstr[y] = s2[b];
+		str[y] = s2[b];
 		y++;
 		b++;
 	}
 	/* Colocamos Null al final del String */
-	newstr[y] = '\0';
-	return (newstr);
+	str[y] = '\0';
+	return (str);
 }
