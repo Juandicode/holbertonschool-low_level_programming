3b5560fc9f1c8d2b3ba434f72706f82c435a0ab9
Juandicode <juandiegomhs@gmail.com>
2024-07-08 17:39:22 +0000

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,10 +3,30 @@
 #include "length.c"
 /**
  * *string_nconcat - Concatenates two strings
- * @ *_calloc - allocates memory for an array
+ * *_calloc - allocates memory for an array
  * @nmemb: variable 1
- * @size: size of the array 
- * Return: Nothing to return 
+ * @size: size of the array
+ * Return: Nothing to return
  */
 
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	return (NULL);
+
+	i = 0;
+	while (i < nmemb * size)
+	{
+	*((char *)ptr + i) = 0;
+	i++;
+	}
+
+	return (ptr);
+}
