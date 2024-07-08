3d143104d8feb2add7c3ef5088bb44c449b67ea8
Juandicode <juandiegomhs@gmail.com>
2024-07-08 17:12:11 +0000

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+#include "length.c"
+/**
+ * *string_nconcat - Concatenates two strings
+ * @ *_calloc - allocates memory for an array
+ * @nmemb: variable 1
+ * @size: size of the array 
+ * Return: Nothing to return 
+ */
+
+
