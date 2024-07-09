84833765f7265bf3de24a5c115e49a952b6b19ed
Juandicode <juandiegomhs@gmail.com>
2024-07-09 19:55:49 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -74,6 +74,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	_strcpy(new_dog->owner, owner);
-
+	new_dog->name = name;
+	new_dog->owner = owner;
+	new_dog->age = age;
 	return (new_dog);
 }
