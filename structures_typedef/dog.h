9c0659784afbccd022909d8322254b02e75a687a
Juandicode <juandiegomhs@gmail.com>
2024-07-10 16:14:00 +0000

    2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,7 +21,7 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 int length(char *str);
 char *_strcpy(char *dest, char *src);
-
+void free_dog(dog_t *d);
 
 #endif	/* DOG_H */
 
