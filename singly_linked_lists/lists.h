ec5a6caa59bf6e23aab8d4b907f359fbc8c1ae20
juandicode <juandiegomhs@gmail.com>
2024-07-29 14:46:21 +0000

    2

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct list_t
+{
+    char *str;
+    unsigned int len;
+    struct list_t *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+
+#endif /* LISTS_H */
