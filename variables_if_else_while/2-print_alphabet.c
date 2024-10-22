c72fc879ae1cba650083d4ad40eddedd2f1cb713
Juandicode <juandiegomhs@mail.com>
2024-10-22 14:36:34 +0000

    abcdario

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char start = 'a';
+
+	for (start = 'a'; start <= 'z'; start++)
+{
+		putchar(start);
+}
+		putchar('\n');
+	return (0);
+}
