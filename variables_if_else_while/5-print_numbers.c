b5c2a5e9234ff041a9fb0e50413abb64bb2edab0
root <root@H40.hbtn.uy>
2024-10-24 10:58:59 -0300

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    :X
+    for (int i = 0; i < 10; i++)
+    {
+        
+        printf("%d", i);
+    }
+    
+    printf("\n");
+    
+    return (0); 
+}
