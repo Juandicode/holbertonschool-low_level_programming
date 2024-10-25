0196c3a45e080495b6a1916efe652fdedcac71f6
Juandicode <juandiegomhs@mail.com>
2024-10-25 11:49:00 -0300

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char pepito[] = "0123456789abcdef";
+	int i = 0;
+
+		for (i = 0; i < 16; i++)
+		{
+			putchar(pepito[i]);
+		}
+		putchar('\n');
+
+	return (0);
+}
