8ec6714f2839d453e326bfca04b5d523daf6bf0e
Juandicode <juandiegomhs@mail.com>
2024-10-31 21:41:27 +0000

    2

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,12 @@
+#·inclue "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle 
+ * @size: the size of the triangle
+ * Return: (0)
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\\');
+}
