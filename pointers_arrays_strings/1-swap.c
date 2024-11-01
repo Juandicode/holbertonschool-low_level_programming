a9eeae4d9946854b1d9999a062b00481772ed291
Juandicode <juandiegomhs@mail.com>
2024-11-01 13:44:57 -0300

    0 res

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first integer variable
+ * @b: second integer variable
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
