1ce20c196a2e864566cf5dd18d97e5ebf9aa182f
Juandicode <juandiegomhs@mail.com>
2024-11-03 13:15:53 +0000

    2

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print a string, followed by a new line
+ *
+ * @str: variable
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}                                                                                                              
+~                        
