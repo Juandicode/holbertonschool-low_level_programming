63937ac1480060c24295298243eae22e65db3f95
Juandicode <juandiegomhs@mail.com>
2024-11-03 23:32:41 +0000

    2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: first variable
+ * @src: second variable
+ * @n: int variable
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+			
+	{
+	
+		char *ptr = dest;
+		int i = 0;
+    
+			while (*ptr != '\0')
+		       	{
+			ptr++;
+			}
+
+			while (*src != '\0' && i < n)
+       			{
+			*ptr = *src;
+			ptr++;
+			src++;
+			i++;
+	}
+			*ptr = '\0';
+
+	return (dest);
+	}
+}
