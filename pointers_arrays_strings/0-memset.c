59e8f7f6ba568e987ac100a9c9bd1e100abf1fa3
Juandicode <juandiegomhs@mail.com>
2024-11-04 22:28:45 +0000

    asignar vbyte b a la posicion del buffer

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes 
+ * _memset - fills the first n bytes
  * @s: pointer to the memory area
  * @b: constant byte to fill the memory with
  * @n: number of bytes to fill
@@ -12,9 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
-			{
+
+		for (i = 0; i < n; i++)
+		{
 			s[i] = b; /* Asigna el byte b a la posición i del buffer*/
-			}
+		}
 	return (s); /* Devuelve el puntero al inicio del buffer */
 }
