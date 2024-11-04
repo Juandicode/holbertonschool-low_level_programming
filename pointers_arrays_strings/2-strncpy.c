6c5a401f144ad6386e535788a0776471c74851b1
Juandicode <juandiegomhs@mail.com>
2024-11-04 00:09:58 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,20 @@
  *
  * Return: A pointer to the destination string `dest`.
  */
-char *_strncpy(char *dest, char *src, int n) {
-    int i;
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+       	{
+	dest[i] = src[i];
+	}
 
-    for (; i < n; i++) {
-        dest[i] = '\0';
-    }
+	for (; i < n; i++)
+       	{
+	dest[i] = '\0';
+	}
 
-    return dest;
+	return (dest);
 }
 
