c70dfcc5b48faa6d4655d2f1deff9887c71732ab
Juandicode <juandiegomhs@gmail.com>
2024-11-13 00:28:23 +0000

    alloc dgrid long betty

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,1 +1,37 @@
-ii
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - return a pointer to a 2dimensional array
+ * @width: width variable
+ * @height: heigt variable
+ * Return: 0
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, i = 0, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (!grid)
+		return (NULL);
+
+	while (i < height)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (!grid[i])
+		{
+			while (i-- > 0)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		j = 0;
+		while (j < width)
+			grid[i][j++] = 0;
+		i++;
+	}
+	return (grid);
+}
