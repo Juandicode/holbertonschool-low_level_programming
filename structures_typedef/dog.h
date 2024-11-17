393fba938419efa3922136fc8a45770c3e5ecc88
Juandicode <juandiegomhs@gmail.com>
2024-11-17 00:00:26 +0000

    haciendo fl 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,12 +2,18 @@
 #define DOG_H
 
 #include <stdio.h>
-
+/**
+ *struct dog - estructura que toma 3 tipos de datos
+ *@owner: owner of the dog
+ *@name: name of the dog
+ *@age: age of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+}
+dog;
 
 #endif /* DOG_H */
