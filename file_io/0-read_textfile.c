a6733f0ab664f3e2f2e2b1c0d96fdf81c2428b97
Juandicode <juandiegomhs@gmail.com>
2025-01-10 20:11:13 +0000

    betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,55 +2,31 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * read_textfile - Lee un archivo de texto y lo imprime en la salida estándar
- * @filename: Nombre del archivo
- * @letters: Número de letras a leer e imprimir
- * Return: Número de letras leídas e impresas, o 0 en caso de error
+ * read_textfile - function
+ * @filename: file
+ * @letters: letters
+ * Return: count
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written, total_written = 0;
-    char *buffer;
+	int i = 0, contar = 0;
+	char *buffer;
 
-    if (!filename)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
-
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    while (total_written < bytes_read)
-    {
-        bytes_written = write(STDOUT_FILENO, buffer + total_written, bytes_read - total_written);
-        if (bytes_written == -1)
-        {
-            free(buffer);
-            close(fd);
-            return (0);
-        }
-        total_written += bytes_written;
-    }
-
-    free(buffer);
-    close(fd);
-    return (total_written);
+	i = open(filename, O_RDONLY);
+	if (i == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	contar = read(i, buffer, letters);
+	contar = write(STDOUT_FILENO, buffer, contar);
+	close(i);
+	free(buffer);
+	return (contar);
 }
-
